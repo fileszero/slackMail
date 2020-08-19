9207243030c36b4a55ea36695a463789837af15f
FILES=0 <files.eq.zero@gmail.com>
2020-08-19 17:05:01 +0900

    dont add #

--- dispatch.py
+++ dispatch.py
@@ -19,6 +19,7 @@ mail=MailParser.MailParser(text)
 # print( mail.get_attr_data() )
 
 converter= defaultConverter.defaultConverter()
+# custom converters
 if mail.from_address=='missed_call_service@eonet.ne.jp':
     converter= missedLalacall.missedLalacall()
 
@@ -28,16 +29,19 @@ msg=converter.convert(mail)
 channel=conf["slack"]["default_channel"]
 match=re.match(r'^slack\+(.*)@', mail.delivered_to)
 if match:
-    channel="#" + match.group(1)
+    channel= match.group(1)
 
 print(msg)
 print(channel)
 
 slack_client = slack.WebClient(conf["slack"]["token"])
 
+# message params
 slack_opt_default={'channel':channel,'text':'slack hello text','as_user':False,'username':'Slack Mail','icon_emoji':':robot_face:'}
 slack_opt= {**slack_opt_default, **msg}
 print(slack_opt)
+
+# post message
 response = slack_client.chat_postMessage(
     channel=slack_opt.get('channel'),  #'#random'
     text=slack_opt.get('text'),
