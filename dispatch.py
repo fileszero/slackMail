fa5b9fd62e3f35b1c809d8947f123005bda2e7aa
FILES=0 <files.eq.zero@gmail.com>
2021-03-03 11:36:15 +0900

    change module location

--- dispatch.py
+++ dispatch.py
@@ -1,52 +1,65 @@
 
 import sys
 import re
+import os
+import select
 
 import slack
 
 import MailParser
 
 import config
-import defaultConverter
-import missedLalacall
+from converters import defaultConverter
+from converters import missedLalacall
 
-text = sys.stdin.read()
+def dispatch( email_file=''):
+    # read mail contents
+    if os.path.exists(email_file):
+        # DEBUG
+        with open(email_file) as f:
+            text = f.read()
+    else:
+        text = sys.stdin.read()
 
-conf=config.get_config()
-# print( text )
-mail=MailParser.MailParser(text)
 
-# print( mail.get_attr_data() )
+    conf=config.get_config()
+    # print( text )
+    mail=MailParser.MailParser(text)
 
-converter= defaultConverter.defaultConverter()
-# custom converters
-if mail.from_address=='missed_call_service@eonet.ne.jp':
-    converter= missedLalacall.missedLalacall()
+    # print( mail.get_attr_data() )
 
-msg=converter.convert(mail)
+    converter= defaultConverter.defaultConverter()
+    # custom converters
+    if mail.from_address=='missed_call_service@eonet.ne.jp':
+        converter= missedLalacall.missedLalacall()
 
-# slack channel
-channel=conf["slack"]["default_channel"]
-match=re.match(r'^slack\+(.*)@', mail.delivered_to)
-if match:
-    channel= match.group(1)
+    msg=converter.convert(mail)
 
-print(msg)
-print(channel)
+    # slack channel
+    channel=conf["slack"]["default_channel"]
+    match=re.match(r'^slack\+(.*)@', mail.delivered_to)
+    if match:
+        channel= match.group(1)
 
-slack_client = slack.WebClient(conf["slack"]["token"])
+    print(msg)
+    print(channel)
 
-# message params
-slack_opt_default={'channel':channel,'text':'slack hello text','as_user':False,'username':'Slack Mail','icon_emoji':':robot_face:'}
-slack_opt= {**slack_opt_default, **msg}
-print(slack_opt)
+    slack_client = slack.WebClient(conf["slack"]["token"])
 
-# post message
-response = slack_client.chat_postMessage(
-    channel=slack_opt.get('channel'),  #'#random'
-    text=slack_opt.get('text'),
-    as_user=slack_opt.get('as_user'),
-    username=slack_opt.get('username'),
-    icon_emoji=slack_opt.get('icon_emoji')
-    )
-print(response)
+    # message params
+    slack_opt_default={'channel':channel,'text':'slack hello text','as_user':False,'username':'Slack Mail','icon_emoji':':robot_face:'}
+    slack_opt= {**slack_opt_default, **msg}
+    print(slack_opt)
+
+    # post message
+    response = slack_client.chat_postMessage(
+        channel=slack_opt.get('channel'),  #'#random'
+        text=slack_opt.get('text'),
+        as_user=slack_opt.get('as_user'),
+        username=slack_opt.get('username'),
+        icon_emoji=slack_opt.get('icon_emoji')
+        )
+    print(response)
+
+dispatch()
+dispatch(os.path.join(os.path.dirname(__file__),"sampledata","lalacall.eml"))
--- googlePeople.py
+++ googlePeople.py
@@ -17,8 +17,10 @@ class GooglePeople:
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
+        token_file=os.path.join(os.path.dirname(__file__),'token.pickle')
+        credentials_file=os.path.join(os.path.dirname(__file__),'credentials.json')
+        if os.path.exists(token_file):
+            with open(token_file, 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -26,11 +28,11 @@ class GooglePeople:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', self.SCOPES)
+                    credentials_file, self.SCOPES)
                 creds = flow.run_console()
                 # creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
+            with open(token_file, 'wb') as token:
                 pickle.dump(creds, token)
 
         self.service = build('people', 'v1', credentials=creds)
